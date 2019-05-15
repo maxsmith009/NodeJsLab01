fb952847e3b4b63a856216a2d89f940ae5184a70
Maksim Zalutski <max03246@gmail.com>
2019-05-15 11:09:16 +0300

    changed error handling

--- index.js
+++ index.js
@@ -1,45 +1,32 @@
 const fs = require('fs');
 
 const createFile = (content) => {
-    try {
-        fs.writeFile('Lab1-Var9-Zalutski-May5-ne-proveriay-mne-strashno.txt', content.join(' '), (err) => {
-            console.log('successfull creation of file');
-        });
-    }
-    catch (err){
-        console.log(err);
-    }
+    fs.writeFile('Lab1-Var9-Zalutski-May5-ne-proveriay-mne-strashno.txt', content.join(' '), (err) => {
+        if (err) throw err;
+        console.log('successfull creation of file');
+    });
 }
 
 const readFile = ([path, ...args]) => {
-    try {
-        fs.readFile(path, 'utf8', (err, content) => {
-            console.log(content);
-        });
-    }
-    catch (err){
-        console.log(err);
-    }   
+    fs.readFile(path, 'utf8', (err, content) => {
+        if (err) throw err;
+        console.log(content);
+    });
 }
 
 const deleteFiles = (files) => {
-    try {
-        files.forEach((path)=>{
-            fs.unlink(path, (err) => {
-                console.log('successfull deletion of files');
-            });
+    files.forEach((path) => {
+        fs.unlink(path, (err) => {
+            if (err) throw err;
+            console.log('successfull deletion of files');
         });
-    }
-    catch (err){
-        console.log(err);
-    }
+    });
 }
 
 const writeFile = ([path, ...content]) =>{
     try {
         fs.appendFileSync(path, `\n${content.join(' ')}`);
-    }
-    catch (err){
+    } catch (err) {
         console.log(err);
     }
 }
@@ -48,16 +35,16 @@ if (process.argv.length > 2){
     let [p1, p2, action, ...args] =  process.argv;
     switch (action) {
         case 'create' :
-            createFile(args)
+            createFile(args);
             break;
         case "delete":
             deleteFiles(args);
             break;
         case "write":
-            writeFile(args)
+            writeFile(args);
             break;
         case "read":
-            readFile(args)
+            readFile(args);
             break;
         default :
             return;
